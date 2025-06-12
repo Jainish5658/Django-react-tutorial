6e29f9712cf4e8c1e087dd990bc72f397a8bbaf0
Jainish <jainish5658@gmail.com>
2025-06-12 15:57:28 +0530

    committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import react from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoutes from "./components/ProtectedRoute"
+
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegsiterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 
 function App() {
    return (
-    <>
-      
-    </>
+    <BrowserRouter> 
+    <Routes>
+      <Route path="/" element={<ProtectedRoutes><Home /></ProtectedRoutes>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<RegsiterAndLogout />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
